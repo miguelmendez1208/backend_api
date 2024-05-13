2a460abe893c0143429895e2e528d51d1eb081bf
miguelmendez1208 <miguelmendez1208@gmail.com>
2024-05-13 04:23:26 -0500

    added bun.serve command.listens to connections will add memory storage later.

--- index.ts
+++ index.ts
@@ -1,1 +1,16 @@
-console.log("Hello via Bun!");
+const data2 = {"name":"adam","email":"adamemail@gmail.com","description":"bad foot"}
+
+const server = Bun.serve({
+    port: 3010,
+    fetch(req) {
+      console.log("New connection!");
+      const jsonResponse = JSON.stringify(data2);
+      return new Response(jsonResponse, {
+        headers: { "Content-Type": "application/json" },
+      });
+    },
+  });
+
+  console.log(`Listening on ${server.url}`);
+  
+  
